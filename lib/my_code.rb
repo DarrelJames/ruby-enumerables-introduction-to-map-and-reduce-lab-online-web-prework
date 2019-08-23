4397de0eecaa2dafad4e47e9588cc4cd238b0e14
Darrel C <dcastellano2007@gmail.com>
2019-08-23 04:43:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,6 +34,9 @@ def map_to_square(array)
   new_array
 end
 
-def reduce_to_total(source_array, starting_point = 0)
+def reduce_to_total(source_array)
+  new = 0
+  source_array.length.times do
 
+  end
 end
