f5dc0462aa2bdda9f79a8150101fa9134a901bc0
Darrel C <dcastellano2007@gmail.com>
2019-08-23 04:29:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,9 +3,7 @@ def map_to_negativize(num)
   new_array = [];
   num.length.times do |index|
     n = num[index]
-    if n > 0
-      new_array << "-#{n}"
+    new_array << n * -1
 
-    end
   end
 end
