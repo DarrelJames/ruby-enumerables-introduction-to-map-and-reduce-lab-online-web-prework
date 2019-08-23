23ec9201f13dbc88d30b895165c949df4e0604f8
Darrel C <dcastellano2007@gmail.com>
2019-08-23 04:51:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,5 +44,10 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(array)
-
+  array.length.times do |index|
+    if !array[index]
+      return false;
+    else
+      true
+    end
 end
