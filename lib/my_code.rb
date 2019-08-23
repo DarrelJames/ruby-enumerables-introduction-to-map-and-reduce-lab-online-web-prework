65798868864824a8f59715bbecb0aec2dda6174f
Darrel C <dcastellano2007@gmail.com>
2019-08-23 04:44:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,7 +34,7 @@ def map_to_square(array)
   new_array
 end
 
-def reduce_to_total(source_array)
+def reduce_to_total(source_array, starting_point = 0)
   new = 0
   source_array.length.times do |index|
     n = source_array[index]
