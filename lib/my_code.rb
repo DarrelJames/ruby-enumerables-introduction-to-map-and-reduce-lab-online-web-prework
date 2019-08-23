50be79279d94b241c16941018ec449a77277c03a
Darrel C <dcastellano2007@gmail.com>
2019-08-23 04:55:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,3 +52,12 @@ def reduce_to_all_true(array)
   end
     return true
 end
+
+def reduce_to_any_true(array)
+  array.length.times do |index|
+    if array[index]
+      return true
+    end
+  end
+  return false
+end
